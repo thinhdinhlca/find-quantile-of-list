0992de953bd65c9c887613e17e193b60f33570c2
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-11-24 08:06:07 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+
+window.function = function (data, sort_keys, delimiter, final_delimiter) {
+  if (data.value === undefined) return undefined;
+  if (sort_keys.value === undefined) return undefined;
+  
+  delimiter = delimiter.value ?? ',';
+  final_delimiter = final_delimiter.value ?? "\n";
+
+  var data_arr = data.value.split(delimiter);
+  var keys_arr = sort_keys.value.split(delimiter);
+  var obj = {};
+  for (i=0; i<data_arr.length; i++) {
+    obj[data_arr[i]] = keys_arr[i];
+  }
+
+  var sorted = Object.keys(obj).sort(function(a,b){return obj[a]-obj[b]});
+
+  var joined = sorted.join(final_delimiter);
+
+  return joined;
+
+}
