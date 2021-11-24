711888057c36f40eebb79e0382ae07d23cd677f3
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-11-24 09:19:28 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,39 +1,12 @@
 
 window.function = function (data, qtl, delimiter) {
   
+  const ss = await import("https://unpkg.com/simple-statistics@7.7.0/dist/simple-statistics.min.js");
+  
   data = data.value ?? "";
   qtl = qtl.value ?? "";
   delimiter = delimiter.value ?? ", ";
-
-  arr = data.split(delimiter);
   
-// sort array ascending
-const asc = arr => arr.sort((a, b) => a - b);
-
-const sum = arr => arr.reduce((a, b) => a + b, 0);
-
-const mean = arr => sum(arr) / arr.length;
-
-// sample standard deviation
-const std = (arr) => {
-    const mu = mean(arr);
-    const diffArr = arr.map(a => (a - mu) ** 2);
-    return Math.sqrt(sum(diffArr) / (arr.length - 1));
-};
-
-const quantile = (arr, q) => {
-    const sorted = asc(arr);
-    const pos = (sorted.length - 1) * q;
-    const base = Math.floor(pos);
-    const rest = pos - base;
-    if (sorted[base + 1] !== undefined) {
-        return sorted[base] + rest * (sorted[base + 1] - sorted[base]);
-    } else {
-        return sorted[base];
-    }
-};
-
-const result = arr => quantile(arr, qtl);
-return result;
+  return ss.quantile(data, 0.33);
 
 }
